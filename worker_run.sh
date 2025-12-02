1eeffc38b406701e1acd4ee1f21434e9a0c0471b
Joel Ethan Batac <bjoelethan@gmail.com>
2025-12-02 20:34:43 +0800

    fix worker_run.sh

--- worker_run.sh
+++ worker_run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 START_URL=$1
-MINUTES=$2
-THREADS=$3
-NS_IP=$4
+WORKER_ID=$2
+MINUTES=$3
+THREADS=$4
+NS_IP=$5
 
 
 echo "[SCRIPT] Starting Worker..."
 python3 main.py worker \
     --start-url "$START_URL" \
-    --worker-id WORKER1 \
+    --worker-id "$WORKER_ID" \
     --threads "$THREADS" \
     --ns-host "$NS_IP" --ns-port 9090 &
 WORKER_PID=$!
